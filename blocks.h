bb6a3c7a0222bf71321b668e07e849cfe81dca4d
flolesko <flolesko@protonmail.com>
2021-04-23 15:48:05 +0200

    adjusted blocks.h

--- blocks.h
+++ blocks.h
@@ -2,16 +2,16 @@
 
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/						/*Update Interval*/	/*Update Signal*/
-	//{"", 		"~/owncloud/Linux/dwmblocks/scripts/weather.sh",	3600,			0},
-	//{"", 		"~/owncloud/Linux/dwmblocks/scripts/gpu.sh",		5,			0},
-	{"", 		"~/owncloud/Linux/dwmblocks/scripts/cpu.sh",		5,			0},
-	{"", 		"~/owncloud/Linux/dwmblocks/scripts/memory.sh",		5,			0},
-	//{"", 		"~/owncloud/Linux/dwmblocks/scripts/vpn.sh",		5,			0},
-	//{"", 		"~/owncloud/Linux/dwmblocks/scripts/network.sh",	5,			0},
-	{"", 		"~/owncloud/Linux/dwmblocks/scripts/audio.sh",		0,			1},
-	{"", 		"~/owncloud/Linux/dwmblocks/scripts/xbacklight.sh",	0,			2},
-	{"", 		"~/owncloud/Linux/dwmblocks/scripts/battery.sh",	5,			0},
-	{"", 		"~/owncloud/Linux/dwmblocks/scripts/date.sh",		5,			0},
+	//{"", 		"~/nextcloud/Linux/dwmblocks/scripts/weather.sh",	3600,			0},
+	//{"", 		"~/nextcloud/Linux/dwmblocks/scripts/gpu.sh",		5,			0},
+	{"", 		"~/nextcloud/Linux/dwmblocks/scripts/cpu.sh",		5,			0},
+	{"", 		"~/nextcloud/Linux/dwmblocks/scripts/memory.sh",		5,			0},
+	//{"", 		"~/nextcloud/Linux/dwmblocks/scripts/vpn.sh",		5,			0},
+	//{"", 		"~/nextcloud/Linux/dwmblocks/scripts/network.sh",	5,			0},
+	{"", 		"~/nextcloud/Linux/dwmblocks/scripts/audio.sh",		0,			1},
+	{"", 		"~/nextcloud/Linux/dwmblocks/scripts/xbacklight.sh",	0,			2},
+	{"", 		"~/nextcloud/Linux/dwmblocks/scripts/battery.sh",	5,			0},
+	{"", 		"~/nextcloud/Linux/dwmblocks/scripts/date.sh",		5,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
